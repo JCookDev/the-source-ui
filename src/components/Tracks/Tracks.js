36fc967ccf150c48b8c3487e27a2db961498930b
Jeff Cook <cookturingedu@gmail.com>
2023-01-04 13:50:00 -0500

    Refactor Tracks component and import new components

--- src/components/Tracks/Tracks.js
+++ src/components/Tracks/Tracks.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import Card from '../Card/Card';
-import '../Tracks/Tracks.css';
+import React from "react";
+import Card from "../Card/Card";
+import MusicGenreRow from "../MusicGenreRow/MusicGenreRow";
+import MusicCardRow from "../MusicCardRow/MusicCardRow";
+import "../Tracks/Tracks.css";
 
 const Tracks = ({ music }) => {
-    const musicCards = music.map(song => {
-        return(
-           <Card 
-                id={song.id}
-                key={song.id}
-                coverArt={song.coverArt}
-                artist={song.artist}
-                genre={song.genre}
-                title={song.title}
-                audioFile={song.audioFile}
-            />
-           
-        );
-    });
+  const rows = [];
+  let lastGenre = null;
 
-    return(
-        <div className='tracks-container'>
-            {musicCards}
-        </div>
+  const musicCards = music.map((song) => {
+    return (
+      <Card
+        id={song.id}
+        key={song.id}
+        coverArt={song.coverArt}
+        artist={song.artist}
+        genre={song.genre}
+        title={song.title}
+        audioFile={song.audioFile}
+      />
     );
+  });
+  console.log("Music Cards", musicCards);
+
+  musicCards.forEach((card) => {
+    if (card.props.genre !== lastGenre) {
+      rows.push(<MusicGenreRow genre={card.props.genre} key={card.props.id} />);
+    };
+    rows.push(<MusicCardRow musicCard={card} key={card.props.name}/>);
+
+      lastGenre = card.props.genre;
+  });
+
+  console.log("ORGANIZED CARDS", rows);
+  return (
+    <div>
+      <table className="tracks-container">
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Tracks;
