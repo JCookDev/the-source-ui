d849db0c77d9b31bd8fdc43e95bc007e9cf8b682
Jeff Cook <cookturingedu@gmail.com>
2023-01-03 23:26:02 -0500

    Add styling to cards component and display music data in the DOM

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../Card/Card.css';
 
-const Card = () => {
+const Card = ({ coverArt, artist, genre, title, audioFile}) => {
   return (
-    <div>
-      
+    <div className='card'>
+      <h2>{artist}</h2>  
+      <p>Cover Art Goes Here!</p>
+      <p> Title: {title} </p>
+      <p>  Genre: {genre} </p>
     </div>
   )
 }
