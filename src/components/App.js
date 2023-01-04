3bddfe31034b88f6090da643bc6144fcc1e3cf6b
Jeff Cook <cookturingedu@gmail.com>
2023-01-03 22:17:42 -0500

    Add dummy data to state

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,39 @@ class App extends Component {
   constructor() {
     super() 
       this.state = {
-
+        music: [
+          {
+            id: 1, 
+            coverArt: "image" , 
+            artist: "Future", 
+            genre: "Hip-Hop",  
+            title: "Puffin On Zooties", 
+            audioFile: "file",
+          },
+          {
+            id: 2, 
+            coverArt: "image" , 
+            artist: "Miss Aphrodite", 
+            genre: "R&B",  
+            title: "Your Hands", 
+            audioFile: "file",
+          },
+          {
+            id: 3, 
+            coverArt: "image", 
+            artist: "Bobby Blue Bland", 
+            genre: "Blues",  
+            title: "Ain't No Sunshine When Shes Gone", 
+            audioFile: "file",
+          }
+        ]
       }
   }
   render () {
     return(
       <main className='App'>
         <h1>The Source</h1>
+        <Tracks />
       </main>
     )
   }
