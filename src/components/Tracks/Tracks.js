a286d09bfce1e23b2682a8aa4d928e73a8b4b6a8
Jeff Cook <cookturingedu@gmail.com>
2023-01-03 22:31:45 -0500

    Create Tracks Component

---
+++ src/components/Tracks/Tracks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import Card from "../Card/Card"
+import "../Tracks/Tracks.css";
+
+const Tracks = () => {
+    return(
+        <div className='Tracks'>
+           <h2>Cards go hear!</h2>
+        </div>
+    );
+}
+
+export default Tracks;
