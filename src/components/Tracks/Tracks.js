7540f61917cdc8d31be5e6de37988b6d0753134d
Jeff Cook <cookturingedu@gmail.com>
2023-01-03 22:47:11 -0500

    Map and pass data to Card component

--- src/components/Tracks/Tracks.js
+++ src/components/Tracks/Tracks.js
@@ -1,11 +1,26 @@
 import React from 'react';
-//import Card from "../Card/Card"
+import Card from "../Card/Card"
 import "../Tracks/Tracks.css";
 
-const Tracks = () => {
+const Tracks = ({ music }) => {
+    const musicCards = music.map(song => {
+        return(
+           <Card 
+                id={song.id}
+                key={song.id}
+                coverArt={song.coverArt}
+                artist={song.artist}
+                genre={song.genre}
+                title={song.title}
+                audioFile={song.audioFile}
+            />
+           
+        );
+    })
+
     return(
         <div className='Tracks'>
-           <h2>Cards go hear!</h2>
+            {musicCards}
         </div>
     );
 }
