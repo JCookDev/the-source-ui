a5f5ed714c770a3ed75c2b82b0fa98512a2a0948
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-04 15:57:50 -0700

    Add form to form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,10 +13,45 @@ class Form extends Component {
     }
   }
 
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
   render() {
     return (
       <form>
-        <p>HELLO!!!!!!!!!!!!</p>
+        <input
+          type="text"
+          placeholder="Enter Cover Art URL"
+          name="coverArt"
+          value={this.state.coverArt}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input
+          type="text"
+          placeholder="Enter Artist Name"
+          name="artist"
+          value={this.state.artist}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <select name="genre" id="genre" onChange={event => this.handleChange(event)}>
+          <option value="">Choose your Genre...</option>
+          <option value="hipHop">Hip-Hop</option>
+          <option value="r&B">R&B</option>
+          <option value="blues">Blues</option>
+        </select>
+
+        <input
+          type="text"
+          placeholder="Enter Audio File Link"
+          name="audioFile"
+          value={this.state.audioFile}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <button>Submit my Music!</button>
       </form>
     )
   }
