4e2983d13d47c623adcd1fa70fb83b3270ff0b4a
Lauren Black <laurenblack5280@gmail.com>
2023-01-04 21:05:36 -0700

    pass tests for cards containing artist names

--- cypress/e2e/home_spec.cy.js
+++ cypress/e2e/home_spec.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
+describe('Home Page', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
   })
+  
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('Should have a title', () => {
+    cy.contains('The Source')
+  })
+
+  it('Should contain all available music', () => {
+    cy.get('.tracks-container').within(() => {
+      cy.get('.card').eq(0).should('contain', 'Future')
+      cy.get('.card').eq(1).should('contain', 'Bob Marley')
+    })
+  })
+      // cy.get('tbody').should('contain', )
+      // .and('contain', 'Puffin on Zooties')
+      // cy.get(':nth-child(2) > td > .card').should('contain', 'Puffin on Zooties')
+      // cy.get(':nth-child(2) > td > .card > :nth-child(3)').should('contain', 'Puffin on Zooties')
+      
+      
+    it('Should organize music by category', () => {
+      cy.get('.tracks-container').within(() => {
+        cy.get('tbody').should('contain', 'Hip Hop')
+      })
+    })    
 })
