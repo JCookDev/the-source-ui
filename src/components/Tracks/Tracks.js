1943872bb4f5b42f1306f65379e5b91bd77647d7
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-10 11:01:24 -0700

    Change styling to NavBar and Tracks components

--- src/components/Tracks/Tracks.js
+++ src/components/Tracks/Tracks.js
@@ -1,42 +1,27 @@
 import React from "react";
 import Card from "../Card/Card";
-// import MusicGenreRow from "../MusicGenreRow/MusicGenreRow";
-// import MusicCardRow from "../MusicCardRow/MusicCardRow";
 import "../Tracks/Tracks.css";
 
-const Tracks = ({ music }) => {
-  // const rows = [];
-  // let lastGenre = null;
+const Tracks = ({ music, removeTrack }) => {
 
-  const musicCards = music.map((song) => {
-    // console.log(song)
+  const musicCards = music.map((song, index) => {
     return (
       <Card
         id={song.id}
         key={song.id}
+        musicIndex={index}
         coverart={song.coverart}
         artist={song.artist}
         genre={song.genre}
         title={song.title}
         audiofile={song.audiofile}
+        removeTrack={removeTrack}
       />
     );
   });
 
-  // musicCards.forEach((card) => {
-  //   if (card.props.genre !== lastGenre) {
-  //     rows.push(<MusicGenreRow genre={card.props.genre} key={card.props.genre} />);
-  //   }
-  //   rows.push(<MusicCardRow musicCard={card} key={card.props.id}/>);
-    
-  //   lastGenre = card.props.genre;
-  // });
-
   return (
     <section className="tracks-section">
-      {/* <table className="tracks-container">
-        <tbody>{rows}</tbody>
-      </table> */}
       <div className="tracks-container">
        {musicCards}
       </div>
